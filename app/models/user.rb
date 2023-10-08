75084184f658bcced33ebf078bcfe8773d74a4c3
yato1122 <nandeyanenaiueo@gmail.com>
2023-10-08 15:01:36 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, presence: true
+         validates :nickname, presence: true
+         validates :first_name, presence: true
+         validates :last_name, presence: true
+         validates :birth_day, presence: true
+
 end
