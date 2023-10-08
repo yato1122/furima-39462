d9b67486065bb76686c1857eb996410b4a368d52
yato1122 <nandeyanenaiueo@gmail.com>
2023-10-08 14:54:50 +0900

    スタイルシートの設置及びルートパスの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  get 'items/index'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  #Defines the root path route ("/")
   root to:"items#index"
   resources :items
 end
