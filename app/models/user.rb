d05de5c71471e7db1b3a7ea535ce962ab8bf1b06
yato1122 <nandeyanenaiueo@gmail.com>
2023-10-15 13:06:08 +0900

    パスワードは、半角英数字混合での入力が必須

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :first_name, presence: true, format: { with: /\p{Hiragana}+|\p{Katakana}+|[\p{Han}\p{Hiragana}\p{Katakana}]+/ }
   validates :first_name, presence: true, format: { with: /\p{Hiragana}+|\p{Katakana}+|[\p{Han}\p{Hiragana}\p{Katakana}]+/ }
-  validates :password,
-            format: { with: /\A(?=.*[A-Za-z])(?=.*\d)/, message: 'must include at least one letter and one digit' }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'must include at least one letter and one digit' }
   validates :last_name, presence: true, format: { with: /\p{Hiragana}+|\p{Katakana}+|[\p{Han}\p{Hiragana}\p{Katakana}]+/ }
   validates :first_name_kana, presence: true, format: { with: /\p{Katakana}+/ }
   validates :last_name_kana, presence: true, format: { with: /\p{Katakana}+/ }
