aa8eeec6c71142d7cd555868ce483430b75c56d9
yato1122 <nandeyanenaiueo@gmail.com>
2023-10-15 20:37:15 +0900

    画像保存の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,8 +13,4 @@ class ItemsController < ApplicationController
 
   def delete
   end
-  
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-  end
 end
