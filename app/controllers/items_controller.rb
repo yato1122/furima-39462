29573299e501e5829a818cd0b5f7ac2beb8b0dba
yato1122 <nandeyanenaiueo@gmail.com>
2023-10-15 20:35:34 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,4 +13,8 @@ class ItemsController < ApplicationController
 
   def delete
   end
+  
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
  belongs_to :user
+ has_one_attached :image
  has_one  :order
 end
