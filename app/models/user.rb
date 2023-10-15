968a75363c2da4e7da63a7a5945de324d2f2b301
yato1122 <nandeyanenaiueo@gmail.com>
2023-10-15 14:48:23 +0900

    正規表現修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\p{Hiragana}+|\p{Katakana}+|[\p{Han}\p{Hiragana}\p{Katakana}]+/ }
-  validates :first_name, presence: true, format: { with: /\p{Hiragana}+|\p{Katakana}+|[\p{Han}\p{Hiragana}\p{Katakana}]+/ }
+  validates :first_name, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}ー]+\z/ }
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'must include at least one letter and one digit' }
-  validates :last_name, presence: true, format: { with: /\p{Hiragana}+|\p{Katakana}+|[\p{Han}\p{Hiragana}\p{Katakana}]+/ }
+  validates :last_name, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}ー]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\p{Katakana}+/ }
   validates :last_name_kana, presence: true, format: { with: /\p{Katakana}+/ }
   validates :birthday, presence: true
