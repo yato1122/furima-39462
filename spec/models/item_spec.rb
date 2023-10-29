f2312e369fca0f22724d15cfffeebf65e2597660
yato1122 <nandeyanenaiueo@gmail.com>
2023-10-29 17:13:36 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,48 +38,57 @@ RSpec.describe Item, type: :model do
       end
 
       it 'カテゴリーに「---」が選択されている場合は出品できない' do
-        @item.category = Category.find_by(name: '---')
-        expect(@item).to_not be_valid
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
 
       it '商品の状態に「---」が選択されている場合は出品できない' do
         @item.condition = Condition.find_by(name: '---')
-        expect(@item).to_not be_valid
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Condition must be other than 1')
       end
 
       it '配送料の負担に「---」が選択されている場合は出品できない' do
         @item.shipping_fee = ShippingFee.find_by(name: '---')
-        expect(@item).to_not be_valid
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping fee must be other than 1')
       end
 
       it '発送元の地域に「---」が選択されている場合は出品できない' do
         @item.shipping_area = ShippingArea.find_by(name: '---')
-        expect(@item).to_not be_valid
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping area must be other than 1')
       end
 
       it '発送までの日数に「---」が選択されている場合は出品できない' do
         @item.shipping_day = ShippingDay.find_by(name: '---')
-        expect(@item).to_not be_valid
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping day must be other than 1')
       end
 
       it '価格に半角数字以外が含まれている場合は出品できない' do
         @item.item_price = '123abc'
-        expect(@item).to_not be_valid
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price is invalid')
       end
 
       it '価格が300円未満では出品できない' do
         @item.item_price = 299
-        expect(@item).to_not be_valid
+        @item.valid?
+        expect(@item.errors[:item_price]).to include('is invalid')
       end
 
       it '価格が9,999,999円を超えると出品できない' do
         @item.item_price = 10_000_000
-        expect(@item).to_not be_valid
+        @item.valid?
+        expect(@item.errors[:item_price]).to include('is invalid')
       end
 
       it 'userが紐付いていなければ出品できない' do
         @item.user = nil
-        expect(@item).to_not be_valid
+        @item.valid?
+        expect(@item.errors[:user]).to include('must exist')
       end
     end
   end
