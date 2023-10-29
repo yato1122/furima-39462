8dc47d0a93b69200d7586e79c4d44317ab49c453
yato1122 <nandeyanenaiueo@gmail.com>
2023-10-29 12:31:56 +0900

    販売手数料修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,11 @@
 window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
   const addTaxDom = document.getElementById("add-tax-price");
   addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
   const addProfitDom = document.getElementById("profit");
   addProfitDom.innerHTML = Math.round(priceInput.value - Math.floor(priceInput.value * 0.1 ))
+})
 });
