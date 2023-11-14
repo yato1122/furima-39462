b1dc575341065a16598ca51a9889db01fce5c207
yato1122 <nandeyanenaiueo@gmail.com>
2023-11-15 00:04:14 +0900

    修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -13,17 +13,16 @@ class OrderAddress
     validates :tell, length: { minimum: 10 }, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
     validates :token
   end
+  def save
+    order = Order.create(item_id: @item_id, user_id: user_id)
+    Address.create(
+      post_code: post_code,
+      municipalities: municipalities,
+      shipping_area_id: shipping_area_id,
+      street_address: street_address,
+      building_name: building_name,
+      tell: tell,
+      order_id: order.id
+    )
 end
-
-def save
-  order = Order.create(item_id: @item_id, user_id: user_id)
-  Address.create(
-    post_code: post_code,
-    municipalities: municipalities,
-    shipping_area_id: shipping_area_id,
-    street_address: street_address,
-    building_name: building_name,
-    tell: tell,
-    order_id: order.id
-  )
 end
